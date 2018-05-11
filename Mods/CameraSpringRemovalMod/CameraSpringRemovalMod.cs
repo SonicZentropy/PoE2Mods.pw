29eec2fb7d26326313268179a26a17a6d5b221bf
Zentropy <Soniczentropy@gmail.com>
2018-05-11 16:25:43 -0500

    Finished camera mod and added unity engine dll

---
+++ Mods/CameraSpringRemovalMod/CameraSpringRemovalMod.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game;
+using Game.UI;
+using Patchwork;
+using UnityEngine;
+
+namespace PoEMods.pw
+{
+    [ModifiesType]
+    public class CameraSpringRemovalMod : SmartCamera
+    {
+        private Vector3 SpringTuneNew
+        {
+            [ModifiesMember("get_SpringTune")]
+            get {
+                return MovementSpringTune * 2f;
+        
+                //Original
+                //if (this.m_mouseBiasBeingTracked)
+                //{
+                //	return this.MovementSpringTune * 2f;
+                //}
+                //if (this.m_timeToUseStrongSpring > 0f)
+                //{
+                //	return this.MovementSpringTune;
+                //}
+                //return this.NoMovementSpringTune;
+            }
+        }
+
+        [ModifiesMember("OnyxLateUpdate")]
+        protected void OnyxLateUpdateNew()
+        {
+            //base.OnyxLateUpdate();
+            this.m_preventTrackingTimer = Mathf.Max(0f, this.m_preventTrackingTimer - Time.deltaTime);
+            m_timeToUseStrongSpring = 50.0f;
+            //m_timeToAllowMovementLookAhead = 0.0f;
+            if (GameInput.GetControl(MappedControl.MOUSE_CAMERA_LOOKAHEAD, false)) {
+                this.m_preventTrackingTimer = 0f;
+                //this.m_timeToUseStrongSpring = this.TimeToAllowStrongSpringMovementAfterNoMovement;
+            }
+            if (!this.IsAnyPartyMemberMoving()) {
+                //this.m_timeToUseStrongSpring = Mathf.Max(0f, this.m_timeToUseStrongSpring - Time.deltaTime);
+                this.m_timeToAllowMovementLookAhead = Mathf.Max(0f, this.m_timeToAllowMovementLookAhead - Time.deltaTime);
+            }
+            if (this.m_mouseBiasBeingTracked) {
+                //this.m_timeToUseStrongSpring = Mathf.Max(this.m_timeToUseStrongSpring, 2f);
+            }
+            for (int i = 0; i < this.m_trackedObjects.Count; i++) {
+                List<GameObject> list = this.m_trackedObjects[(SmartCamera.TrackingTargetType)i];
+                for (int j = list.Count - 1; j >= 0; j--) {
+                    if (list[j] == null || !list[j].activeSelf) {
+                        list.RemoveAt(j);
+                    }
+                }
+            }
+            for (int k = this.m_delayedRemoveObjectHashes.Count - 1; k >= 0; k--) {
+                List<float> delayedRemoveObjectTimers;
+                int index;
+                (delayedRemoveObjectTimers = this.m_delayedRemoveObjectTimers)[index = k] = delayedRemoveObjectTimers[index] - Time.deltaTime;
+                if (this.m_delayedRemoveObjectTimers[k] <= 0f) {
+                    this.RemoveGameObjectToFollow(this.m_delayedRemoveObjectHashes[k], this.m_delayedRemoveObjectTypes[k]);
+                    this.m_delayedRemoveObjectHashes.RemoveAt(k);
+                    this.m_delayedRemoveObjectTimers.RemoveAt(k);
+                    this.m_delayedRemoveObjectTypes.RemoveAt(k);
+                }
+            }
+            this.m_timeUntilRefilterPartyTracking -= TimeController.UnscaledDeltaTime;
+            if (this.m_timeUntilRefilterPartyTracking <= 0f) {
+                this.FilterPartyMembersToTrack();
+            }
+            if (this.LerpFocusPosition) {
+                Vector3 hitPoint = this.GetHitPoint(this.GetFocusWorldLocation());
+                this.m_currentFocusPosition = Vector3.Lerp(this.m_currentFocusPosition, hitPoint, Mathf.Clamp01(this.GetDeltaTime() * (this.CurrentFocusLerpStrengh)));
+                //this.m_currentFocusPosition = Vector3.Lerp(this.m_currentFocusPosition, hitPoint, 1.0f);
+            }
+            else {
+                this.m_currentFocusPosition = this.GetHitPoint(this.GetFocusWorldLocation());
+            }
+            if (SmartCamera.DrawDebug) {
+                this.DrawCameraDebug();
+            }
+        }
+    }
+}
+
+
---
+++ Mods/MulticlassPenaltyRemover/MulticlassPenaltyRemover.cs
@@ -0,0 +1,80 @@
+﻿/** 
+ * GameSpeedMod.cs
+ * Dylan Bailey
+ * 11/14/16
+ * License - Do whatever you want with this code as long as no puppies are set aflame
+*/
+
+using Game;
+using Game.UI;
+using Patchwork;
+using UnityEngine;
+//using SDK;
+
+namespace TyrannyMods.pw
+{
+    /// <summary>
+    /// Modifies the game speed toggles to add a new 6x speed.
+    /// </summary>
+    [ModifiesType]
+    public class CharacterProgressionGameDataNew : Game.GameData.CharacterProgressionGameData
+    {            
+
+        [ModifiesMember("GetClassPowerLevel")]
+        public int GetClassPowerLevelNew(bool isMulticlassed, int characterLevel)
+		{
+            // this is literally the only required changed line
+            isMulticlassed = false; 
+			int charLevelAsIndex = characterLevel - 1;
+			if (charLevelAsIndex < 0)
+			{
+				return 0;
+			}
+			if (isMulticlassed)
+			{
+				if (charLevelAsIndex >= this.MultiClassPowerLevelByCharacterLevel.Count)
+				{
+					UIDebug.LogOnScreenWarning(string.Concat(new object[]
+					{
+						"Multi Class Power Level By Character Level list only has ",
+						this.MultiClassPowerLevelByCharacterLevel.Count,
+						" entries. Trying to lookup character with class level: ",
+						characterLevel
+					}), UIDebug.Department.Design, 10f);
+					return this.MultiClassPowerLevelByCharacterLevel[this.MultiClassPowerLevelByCharacterLevel.Count - 1];
+				}
+				return this.MultiClassPowerLevelByCharacterLevel[charLevelAsIndex];
+			}
+			else
+			{
+				if (charLevelAsIndex >= this.SingleClassPowerLevelByCharacterLevel.Count)
+				{
+					UIDebug.LogOnScreenWarning(string.Concat(new object[]
+					{
+						"Single Class Power Level By Character Level list only has ",
+						this.SingleClassPowerLevelByCharacterLevel.Count,
+						" entries. Trying to lookup character with class level: ",
+						characterLevel
+					}), UIDebug.Department.Design, 10f);
+					return this.SingleClassPowerLevelByCharacterLevel[this.MultiClassPowerLevelByCharacterLevel.Count - 1];
+				}
+				return this.SingleClassPowerLevelByCharacterLevel[charLevelAsIndex];
+			}
+		}
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
