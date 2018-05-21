3f59bb092c77b6a4fa579e9cf290db53d5454ab6
Dylan Bailey <soniczentropy@gmail.com>
2018-05-21 00:11:36 -0500

    Free surrender mod added

---
+++ Mods/PoE2Mods/FreeSurrenderMod/FreeSurrenderMod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game;
+using Patchwork;
+
+namespace PoE2Mods
+{
+    [ModifiesType("Game.ShipDuelManager")]
+    class FreeSurrenderMod
+    {
+        [ModifiesMember("DeductSurrenderCost")]
+        public void DeductSurrenderCostNew() {
+            return;
+        }
+
+    }
+}
