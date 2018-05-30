49bf54d142653e0521497847b0ee97376aa981b6
Dylan Bailey <soniczentropy@gmail.com>
2018-05-29 22:02:35 -0500

    Added fog of war mod

---
+++ Mods/PoE2Mods/FogOfWarMod/FogOfWarMod.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Patchwork;
+using UnityEngine;
+
+namespace PoE2Mods
+{
+    [ModifiesType("Game.FogOfWar")]
+    class FogOfWarMod : Game.FogOfWar
+    {
+        [NewMember]
+        bool ConfigHasBeenInit;
+
+        [NewMember]
+        bool UseMod;
+
+        [NewMember]
+        public void InitMods() {
+            UseMod = UserConfig.GetValueAsBool("FogOfWarMod", "enableMod");
+            ConfigHasBeenInit = true;
+        }
+
+        [ModifiesMember("PointFullyVisible")]
+        public bool PointFullyVisibleNew(Vector3 worldPosition)
+        {
+            
+            if (!FogOfWarRender.FogOfWarRendering)
+            {
+                return true;
+            }
+            if (!ConfigHasBeenInit) InitMods();
+
+            int num;
+            int num2;
+            this.WorldToFogOfWar(worldPosition, out num, out num2);
+            if (UseMod) 
+                return num >= 0 && num2 >= 0 && num < this.m_fogVertsPerColumn && num2 < this.m_fogVertsPerRow && this.m_fogAlphas[num * this.m_fogVertsPerRow + num2].DisplayedAlpha - 0.01f < 0.7f;
+            else {
+                return num >= 0 && num2 >= 0 && num < this.m_fogVertsPerColumn && num2 < this.m_fogVertsPerRow && this.m_fogAlphas[num * this.m_fogVertsPerRow + num2].DisplayedAlpha - 0.01f < 0.05f;
+            }
+        }
+    }
+}
